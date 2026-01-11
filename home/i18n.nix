9e41241d75e4921c84a13b34489bd4a9a2598f24
SewoShun <93058265+SewoShun@users.noreply.github.com>
2026-01-11 16:06:43 +0900

    update fcitx5 config

--- home/i18n.nix
+++ home/i18n.nix
@@ -9,6 +9,21 @@
         fcitx5-skk
       ];
       waylandFrontend = true;
+      settings = {
+        globalOptions = {
+          "Hotkey/TriggerKeys"."0" = "Super+space";
+        };
+        inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "hazkey";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "hazkey";
+        };
+      };
     };
   };
 }
