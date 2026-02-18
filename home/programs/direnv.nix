33f8f334bbb8a1b948f03128074bc1c31a092f73
SewoShun <93058265+SewoShun@users.noreply.github.com>
2026-02-18 17:18:45 +0900

    modify direnv config

--- home/programs/direnv.nix
+++ home/programs/direnv.nix
@@ -2,5 +2,11 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    stdlib = ''
+      declare -A direnv_layout_dirs
+      direnv_layout_dir() {
+        echo /tmp/direnv/$(pwd | base64)
+      }
+    '';
   };
 }
