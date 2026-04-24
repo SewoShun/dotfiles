6d8f952510f2905c2832e7525a471d9ef2617754
SewoShun <93058265+SewoShun@users.noreply.github.com>
2026-04-24 09:59:21 +0900

    remove systemd-boot settings in desktop

--- hosts/desktop/nixos/boot.nix
+++ hosts/desktop/nixos/boot.nix
@@ -7,10 +7,6 @@
       "nvidia_uvm"
       "nvidia_drm"
     ];
-
-    loader.systemd-boot = {
-      enable = true;
-    };
     kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_latest;
   };
 }
